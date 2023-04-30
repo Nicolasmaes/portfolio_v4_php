63e4cde223e4172d4e498d0bf8b40355769b6d2d
Nicolas Maes <contact@nicolasmaes.fr>
2023-04-30 20:58:09 +0200

    ci cd hostinger test

--- front/confirmation/index.php
+++ front/confirmation/index.php
@@ -8,8 +8,23 @@
     <script src="https://kit.fontawesome.com/d77ac583c4.js" crossorigin="anonymous"></script>
     <link rel="icon" href="../../favicon/favicon.ico" />
     <link rel="stylesheet" href="../../styles/style.css">
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gt6ucqzgoc");
+</script>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TPRMP87X5S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-TPRMP87X5S');
+</script>
 <body>
     <div class="card" data-state="#about">
         <div class="card-header">
--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,22 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="../styles/style.css">
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gt6ucqzgoc");
+</script>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TPRMP87X5S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TPRMP87X5S');
+</script>
 
 <body>
